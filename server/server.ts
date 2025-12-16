571621237dab77bb26bb16d739bc468233d74a35
Nikunj Saini <nikunj902782@gmail.com>
2025-12-16 21:06:38 +0530

    feat:

--- server/server.ts
+++ server/server.ts
@@ -1,13 +1,16 @@
-import express, { Request, Response } from 'express';
-import 'dotenv/config';
-import cors from 'cors';
-const app = express();
+import express, { Request, Response } from 'express'; 
 
-const port = 3000;
+import 'dotenv/config'
+import cors from 'cors'
+const app = express();
 const corsOptions = {
     origin : process.env.Trusted_url?.split(',') || [] , 
     credentials : true , 
 }
+app.use(cors())
+
+const port = 3000;
+
 app.get('/', (req: Request, res: Response) => {
     res.send('Server is Live!');
 });
