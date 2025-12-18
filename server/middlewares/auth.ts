5a655f209342763301fce772df791827f8a7bd9a
Nikunj Saini <nikunj902782@gmail.com>
2025-12-18 20:28:28 +0530

    feat: auth ts middleware

---
+++ server/middlewares/auth.ts
@@ -0,0 +1,22 @@
+import { Request , Response , NextFunction,  } from "express"
+import { auth } from "../lib/auth.js"
+import { fromNodeHeaders } from "better-auth/node"
+export const protect = async (req : Request , res: Response , next  : NextFunction) =>{
+    try {
+        const session = await auth.api.getSession({
+            headers : fromNodeHeaders(req.headers)
+        })
+        if(!session || !session?.user){
+            return res.status(401).json({
+                message : 'Unauthorised User'
+            })
+        }
+        req.userId = session.user.id;
+        next()
+    }
+    catch(error : any ){
+        return res.json({
+             success : false , message : error.message
+        })
+    }
+}
